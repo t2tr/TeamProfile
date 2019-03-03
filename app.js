8c22971f4418ddda102cdcb7a45776da12682069
trungdinhle <33343210+trungdinhle@users.noreply.github.com>
2019-03-03 19:41:27 +0700

    fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 const PORT=3000; 
 
-fs.readFile('./default.html', function (err, html) {
+fs.readFile('./index.html', function (err, html) {
 
     if (err) throw err;    
 
